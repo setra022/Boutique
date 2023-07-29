93f873d32e4c82ab8742bbfd704f27698b47b4e6
Setra Rakotovao <setra.rakotovao@student-cs.fr>
2023-07-29 22:18:09 +0200

    Create model Product

--- store/models.py
+++ store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
+    description = models.TextField(blank=True)
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
