47abb81210d15ca8b348f488577ac0b584775e7e
Setra Rakotovao <setra.rakotovao@student-cs.fr>
2023-07-30 10:29:51 +0200

    Add image in article

--- store/models.py
+++ store/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Article(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="articles", null=True, blank=True)
     
     def __str__(self):
         return self.name
