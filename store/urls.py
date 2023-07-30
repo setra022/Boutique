8ad08eb85da49d10c1ea74c60d134055846d0bbe
Setra Rakotovao <setra.rakotovao@student-cs.fr>
2023-07-30 23:20:03 +0200

    use generic views

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'store'
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:article_id>", views.detail, name="detail")
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>", views.DetailView.as_view(), name="detail")
 ]
--- store/views.py
+++ store/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import get_object_or_404, render
 from .models import Article
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
-def index(request):
-    articles = Article.objects.all()
-    context = {"articles": articles}
-    return render(request, "store/index.html", context)
 
-def detail(request, article_id):
-    article = get_object_or_404(Article, pk=article_id)
-    context = {"article": article}
-    return render(request, "store/detail.html", context)
+class IndexView(ListView):
+    model = Article
+
+
+class DetailView(DetailView):
+    model = Article
+
