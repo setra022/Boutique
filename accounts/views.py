3c62d65d61c92e1aad9cea4dc834b66e3467cc4f
Setra Rakotovao <setra.rakotovao@student-cs.fr>
2023-08-06 18:40:46 +0200

    Improve signup form

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,10 @@ def signup(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
-        user = User.objects.create_user(username=username, password=password)
+        first_name = request.POST.get("first-name")
+        last_name = request.POST.get("last-name")
+        email = request.POST.get("email")
+        user = User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name, email=email)
         login(request, user)
         return redirect('store:index')
     return render(request, 'accounts/signup.html')
